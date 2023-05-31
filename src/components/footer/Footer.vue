aed384ebbf417e71b7b800673a90f78039588ebf
trunglk-ibl <trunglk@blockchainlabs.asia>
2023-05-31 17:48:17 +0700

    update footer another page

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -101,55 +101,55 @@
           <div class="md:order-2">
             <div class="flex lg:flex-col md:justify-end">
               <div class="flex gap-3 md:justify-end">
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Twitter"
-                  external
-                  noStyle
-                >
-                  <IconTwitter />
-                </BalLink>
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Discord"
-                  external
-                  noStyle
-                >
-                  <IconDiscord />
-                </BalLink>
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Medium"
-                  external
-                  noStyle
-                >
-                  <IconMedium />
-                </BalLink>
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Youtube"
-                  external
-                  noStyle
-                >
-                  <IconYoutube />
-                </BalLink>
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Github"
-                  external
-                  noStyle
-                >
-                  <IconGithub />
-                </BalLink>
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Linkedin"
-                  external
-                  noStyle
-                >
-                  <IconLinkedin />
-                </BalLink>
-                <BalLink
-                  :href="EXTERNAL_LINKS.Balancer.Social.Mail"
-                  external
-                  noStyle
-                >
-                  <IconMail />
-                </BalLink>
+               <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Twitter"
+              external
+              noStyle
+            >
+              <IconTwitter />
+            </BalLink>
+            <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Discord"
+              external
+              noStyle
+            >
+              <IconDiscord />
+            </BalLink>
+            <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Medium"
+              external
+              noStyle
+            >
+              <IconMedium />
+            </BalLink>
+            <!-- <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Youtube"
+              external
+              noStyle
+            >
+              <IconYoutube />
+            </BalLink> -->
+            <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Github"
+              external
+              noStyle
+            >
+              <IconGithub />
+            </BalLink>
+            <!-- <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Linkedin"
+              external
+              noStyle
+            >
+              <IconLinkedin />
+            </BalLink> -->
+            <!-- <BalLink
+              :href="EXTERNAL_LINKS.Balancer.Social.Mail"
+              external
+              noStyle
+            >
+              <IconMail />
+            </BalLink> -->
               </div>
             </div>
           </div>
