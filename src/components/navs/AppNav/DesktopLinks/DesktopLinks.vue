c4f68ee87046f5ed15f0c83bc5b61f09b8f5a531
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-02-02 18:12:09 +0700

    update bridge external link

--- src/components/navs/AppNav/DesktopLinks/DesktopLinks.vue
+++ src/components/navs/AppNav/DesktopLinks/DesktopLinks.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router';
-import useWeb3 from '@/services/web3/useWeb3';
-import DesktopLinkItem from './DesktopLinkItem.vue';
-import useNetwork from '@/composables/useNetwork';
 import { Goals, trackGoal } from '@/composables/useFathom';
+import useNetwork from '@/composables/useNetwork';
 import { NAV_LINKS } from '@/constants/navLinks';
+import useWeb3 from '@/services/web3/useWeb3';
+import { useRoute } from 'vue-router';
+import DesktopLinkItem from './DesktopLinkItem.vue';
 
 const { isGoerli } = useWeb3();
 
@@ -17,9 +17,9 @@ const { networkSlug } = useNetwork();
 const navLinks = NAV_LINKS.map(i => {
   return {
     ...i,
-    goal: Goals[i.goal_key]
-  }
-})
+    goal: Goals[i.goal_key],
+  };
+});
 
 /**
  * METHODS
@@ -41,11 +41,36 @@ function isActive(page: string): boolean {
     >
       {{ $t(i.text) }}
     </DesktopLinkItem>
+    <a
+      href="https://app.tealswap.com/bridge/oasys-verse"
+      class="border-white dark:border-gray-900 desktop-link-item-custom"
+      target="_blank"
+      >Bridge</a
+    >
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .desktop-links {
   @apply grid gap-6 grid-flow-col grid-rows-1 h-full content-center;
+  .desktop-link-item-custom {
+    position: relative;
+    display: flex;
+    height: 100%;
+    cursor: pointer;
+    flex-direction: column;
+    justify-content: center;
+    overflow: hidden;
+    padding: 0px;
+    --tw-text-opacity: 1;
+    color: rgb(255 255 255 / var(--tw-text-opacity));
+    transition-property: all;
+    transition-duration: 500ms;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    &:hover {
+      --tw-text-opacity: 1;
+      color: rgb(71 85 105 / var(--tw-text-opacity));
+    }
+  }
 }
 </style>
