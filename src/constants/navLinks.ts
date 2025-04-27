cd74da1ff6de742200bd6f41589c494d12b811e6
trunglk-ibl <trunglk@blockchainlabs.asia>
2025-04-27 11:08:13 +0700

    update NavLinks support by chains

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,5 +1,4 @@
-const isTestnet = import.meta.env.VITE_IS_TESTNET == 'true' || 'false';
-let NAV_LINKS_MAINNET = [
+const NAV_LINKS_MAINNET = [
   {
     text: 'pool',
     path: 'pool',
@@ -7,59 +6,34 @@ let NAV_LINKS_MAINNET = [
     goal_key: 'ClickNavPools',
   },
   { text: 'swap', path: 'swap', name_link: 'swap', goal_key: 'ClickNavSwap' },
-  // {
-  //   text: 'claim',
-  //   path: 'claim',
-  //   name_link: 'claim',
-  //   goal_key: 'ClickNavClaim',
-  // },
-  // {
-  //   text: 'portfolio',
-  //   path: 'portfolio',
-  //   name_link: 'portfolio',
-  //   goal_key: 'ClickNavPortfolio',
-  // },
+  {
+    text: 'claim',
+    path: 'claim',
+    name_link: 'claim',
+    goal_key: 'ClickNavClaim',
+    chainsSupport: [17117, 9372],
+  },
+  {
+    text: 'portfolio',
+    path: 'portfolio',
+    name_link: 'portfolio',
+    goal_key: 'ClickNavPortfolio',
+    chainsSupport: [17117, 9372],
+  },
   {
     text: 'sZ',
     path: 'sZ',
     name_link: 'vebal',
     goal_key: 'ClickNavVebal',
-    chainsSupport: [248],
+    chainsSupport: [17117, 9372, 248],
   },
   {
     text: 'Bridge',
     path: 'bridge',
     name_link: 'bridge',
     goal_key: 'ClickNavBridge',
+    chainsSupport: [248, 16116],
   },
 ];
-if (isTestnet == true) {
-  NAV_LINKS_MAINNET = [
-    {
-      text: 'pool',
-      path: 'pool',
-      name_link: 'list-pool',
-      goal_key: 'ClickNavPools',
-    },
-    { text: 'swap', path: 'swap', name_link: 'swap', goal_key: 'ClickNavSwap' },
-    {
-      text: 'claim',
-      path: 'claim',
-      name_link: 'claim',
-      goal_key: 'ClickNavClaim',
-    },
-    {
-      text: 'portfolio',
-      path: 'portfolio',
-      name_link: 'portfolio',
-      goal_key: 'ClickNavPortfolio',
-    },
-    {
-      text: 'sZ',
-      path: 'sZ',
-      name_link: 'vebal',
-      goal_key: 'ClickNavVebal',
-    },
-  ];
-}
+
 export const NAV_LINKS = [...NAV_LINKS_MAINNET];
