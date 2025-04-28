9328dc5f2b916b30a1b2964d20a25ee5cccc746b
Hung Vu <hungvm@blockchainlabs.asia>
2025-04-28 10:49:13 +0700

    Fix bug

--- src/pages/claim/index.vue
+++ src/pages/claim/index.vue
@@ -122,7 +122,8 @@ const balRewardsData = computed((): RewardRow[] => {
   if (!isWalletReady.value) return [];
   // Using reduce to filter out gauges we don't have corresponding pools for
   return gauges.value.reduce<RewardRow[]>((arr, gauge) => {
-    const amount = formatUnits(gauge.claimableTokens, balToken.value.decimals);
+    const decimals = balToken && balToken.value ? balToken.value.decimals : 18;
+    const amount = formatUnits(gauge.claimableTokens, decimals);
     const pool = gaugePools.value.find(pool => pool.id === gauge.poolId);
 
     if (pool && bnum(amount).gt(0))
