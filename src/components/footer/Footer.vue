edca0b8489927bc4bd0977dd78a5aab0a6808ff6
trunglk-ibl <trudyan141@gmail.com>
2025-06-24 12:33:00 +0700

    update version for extraRewardCard

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -138,7 +138,7 @@
         </div>
       </div>
       <div class="pb-12 text-sm text-white dark:text-gray-400">
-        Version 1.89.10
+        Version 1.89.11
       </div>
     </div>
   </footer>
