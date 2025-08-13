0379bf4ac46479ea363d95bbce06980f0ca8a462
trunglk-ibl <trudyan141@gmail.com>
2025-08-13 18:17:35 +0700

    chores: hide defiverse when bridge To

--- src/components/contextual/pages/bridge/BridgeCard/BridgeCard.vue
+++ src/components/contextual/pages/bridge/BridgeCard/BridgeCard.vue
@@ -745,6 +745,11 @@ function checkInputToChange() {
   );
   inputToSelect.value.chainsList = dstBE.value;
 
+  // Filter out Defiverse (chain ID 16116) from chainTo options
+  inputToSelect.value.chainsList = inputToSelect.value.chainsList.filter(
+    (chain: any) => chain.chain_id_decimals !== 16116
+  );
+
   // sort order
   inputToSelect.value.chainsList = sortArrayByReference(
     inputToSelect.value.chainsList,
@@ -778,12 +783,8 @@ function checkInputToChange() {
   } else {
     // set default chainId for inputTo
     if (inputToSelect.value.chainsList.length > 0) {
-      let avaiChain: any = inputToSelect.value.chainsList.find(
-        (chain: any) => chain.chain_id_decimals === 16116 // set Defiverse is default
-      );
-      if (!avaiChain) {
-        avaiChain = inputToSelect.value.chainsList[0];
-      }
+      // Since Defiverse is filtered out, use the first available chain as default
+      let avaiChain: any = inputToSelect.value.chainsList[0];
       inputToSelect.value.chainId = avaiChain.chain_id_decimals;
       //inputToSelect.value.tokenSymbol = avaiChain.tokens[0]?.symbol;
       inputToSelect.value.tokenAddress = avaiChain.tokens.find(
