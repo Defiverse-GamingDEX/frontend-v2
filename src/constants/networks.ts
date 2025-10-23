b7a3058314daeaa59582e5e24d6be9781500e735
trunglk-ibl <trudyan141@gmail.com>
2025-10-23 15:25:29 +0700

    feat: update remove defiverse for mainnet and defiverse-testnet for testnet

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -24,16 +24,16 @@ let networks: any = [
   // //   networkSlug: 'arbitrum',
   // //   key: '42161',
   // // },
-  {
-    id: 'defiverse',
-    name: 'Defiverse',
-    networkSlug: 'defiverse',
-    key: '16116',
-    tokens: defiverseJson.tokens,
-    // price: 5000000000000, // 5000 Gwei
-    // gasUnit: 'wei',
-    maxPriorityFee: 0,
-  },
+  // {
+  //   id: 'defiverse',
+  //   name: 'Defiverse',
+  //   networkSlug: 'defiverse',
+  //   key: '16116',
+  //   tokens: defiverseJson.tokens,
+  //   // price: 5000000000000, // 5000 Gwei
+  //   // gasUnit: 'wei',
+  //   maxPriorityFee: 0,
+  // },
   // // bridge networks
   // {
   //   id: 'mchverse',
@@ -98,16 +98,16 @@ let networks: any = [
 ];
 
 let networksDev = [
-  {
-    id: 'defiverse-testnet',
-    name: 'DefiVerse-Testnet',
-    networkSlug: 'defiverse-testnet',
-    key: '17117',
-    tokens: defiverseTestnetJson.tokens,
-    price: 35000000000, // 50 Gwei
-    gasUnit: 'wei',
-    maxPriorityFee: 0,
-  },
+  // {
+  //   id: 'defiverse-testnet',
+  //   name: 'DefiVerse-Testnet',
+  //   networkSlug: 'defiverse-testnet',
+  //   key: '17117',
+  //   tokens: defiverseTestnetJson.tokens,
+  //   price: 35000000000, // 50 Gwei
+  //   gasUnit: 'wei',
+  //   maxPriorityFee: 0,
+  // },
   {
     id: 'oasys-testnet',
     name: 'Oasys-Testnet',
