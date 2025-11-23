c844ba12df57e236ae5a39afecbc160be202a56e
Hung Vu <hungvm@blockchainlabs.asia>
2025-11-24 00:57:46 +0700

    Add Vote Incentives

--- src/composables/queries/useApiGaugesQuery.ts
+++ src/composables/queries/useApiGaugesQuery.ts
@@ -9,6 +9,7 @@ import { SubgraphGauge } from '@/services/balancer/gauges/types';
 import axios from 'axios';
 const MAINNET_API_URL = 'https://price-api.gaming-dex.com/api';
 const TESTNET_API_URL = 'https://price-api-testnet.gaming-dex.com/api';
+// const TESTNET_API_URL = 'http://localhost:3001/api';
 
 const isTestnet = import.meta.env.VITE_IS_TESTNET == 'true' || 'false';
 const domain = isTestnet == 'false' ? MAINNET_API_URL : TESTNET_API_URL;
