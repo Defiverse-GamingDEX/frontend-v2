b96471adeca5fbef7c2eeb3f6f4d14ac268efa45
trunglk-ibl <trudyan141@gmail.com>
2025-12-03 12:59:04 +0700

    fix for wrong active chain from localstorage

--- src/composables/useNetwork.ts
+++ src/composables/useNetwork.ts
@@ -9,17 +9,27 @@ import { RouteParamsRaw } from 'vue-router';
  * STATE
  */
 const windowAvailable = typeof window !== 'undefined';
-const localStorageNetworkId: Network | null =
+const IS_TESTNET = import.meta.env.VITE_IS_TESTNET === 'true';
+let localStorageNetworkId: Network | null =
   windowAvailable && localStorage.getItem('networkId')
     ? (Number(localStorage.getItem('networkId')) as Network)
     : null;
+if (IS_TESTNET) {
+  if(localStorageNetworkId !== Network.MEGAETH_TESTNET){
+    localStorageNetworkId = Network.MEGAETH_TESTNET;
+  }
+}else {
+  if(localStorageNetworkId !== Network.OASYS){
+    localStorageNetworkId = Network.OASYS;
+  }
+}
 const routeSlug =
   (windowAvailable && window.location.hash.split(/[/?]/)[1]) ?? '';
 const urlNetworkId: Network | null = routeSlug
   ? networkFromSlug(routeSlug)
   : null;
 
-const IS_TESTNET = import.meta.env.VITE_IS_TESTNET === 'true';
+
 const DEFAULT_NETWORK = IS_TESTNET ? Network.MEGAETH_TESTNET : Network.OASYS;
 
 const NETWORK_ID =
