786aa97d8ac31a020542903464e3756e1b5f6a05
Hung Vu <hungvm@blockchainlabs.asia>
2026-01-20 09:29:05 +0700

    Update

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -103,9 +103,9 @@ let networks: any = [
     networkSlug: 'megaeth',
     key: '4326',
     tokens: megaethJson.tokens,
-    // price: 35000000000, // 50 Gwei
+    price: 1100000, // 50 Gwei
     gasUnit: 'wei',
-    maxPriorityFee: 0,
+    // maxPriorityFee: 0,
   },
 ];
 
