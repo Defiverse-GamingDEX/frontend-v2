24a131c16dfa469199ec08e449d096734ce0c926
Hung Vu <hungvm@blockchainlabs.asia>
2026-01-21 05:13:44 +0700

    Update

--- src/components/modals/SelectTokenModal/SelectTokenModal.vue
+++ src/components/modals/SelectTokenModal/SelectTokenModal.vue
@@ -132,7 +132,7 @@ const tokens = computed(() => {
     };
   });
 
-  /*    
+  /*
     2023-12-16: Change request
     ====================================================================================================
     Is it currently designed so that all tokens held are loaded and displayed on the Token list?
@@ -162,7 +162,7 @@ const tokens = computed(() => {
 const excludedTokens = computed(() => [
   ...props.excludedTokens,
   ...(props.includeEther ? [] : [nativeAsset.address]),
-  configService.network.addresses.veBAL,
+  // configService.network.addresses.veBAL,
 ]);
 
 const focussedTokenAddress = computed((): string => {
@@ -466,5 +466,3 @@ watchEffect(() => {
   background-color: #374151;
 }
 </style>
-
-
