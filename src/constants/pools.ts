d25532ffb59cda9ecbd7a64be8d7bbaef6bf8911
Hung Vu <hungvm@blockchainlabs.asia>
2026-02-27 13:16:50 +0700

    Update

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -874,6 +874,7 @@ const POOLS_MEGAETH_TESTNET: Pools = {
   },
   Factories: {
     '0x84f75879c0ac87eafaf4f1e3e1a2372caab5273b': 'weightedPool', // WeightedPool V3
+    '0x8cdbb94fc25c4c94c60664b63f6df452776f9d5d': 'weightedPool', // WeightedPool V3
   },
   Stakable: {
     AllowList: [],
